d10493bc78122c30044ae0cde819cb93786fe665
habenz <haben.zaidt@gmail.com>
2025-01-03 00:52:17 -0500

    rough out markup for task component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import "./App.css";
+import styles from "./App.module.css";
+import tasks from "./data/tasks.json";
+import Task from "./Task";
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <div className={styles.centered}>
+      <h1 className={styles.content}>Checklist</h1>
+      <Task task={{ ...tasks.opening[1], expanded: true }} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Task.js
@@ -0,0 +1,42 @@
+import styles from "./Task.module.css";
+import appStyles from "./App.module.css";
+
+function Task({
+  task: { expanded, complete, title, subtasks },
+  toggleCompletion,
+}) {
+  subtasks.forEach((st) => console.log(st.title));
+  return (
+    <div className={`${styles.task} ${appStyles.content}`}>
+      <div className={styles.title}>
+        <input
+          type="checkbox"
+          checked={complete}
+          aria-label={`Mark ${title} as ${
+            complete ? "incomplete" : "complete"
+          }`}
+        />
+        <h3>{title}</h3>
+        <button onClick={toggleCompletion}>{expanded ? "^" : "v"}</button>
+      </div>
+      {/* really just because I don't trust myself to hand write json */}
+      {subtasks?.length > 0 && expanded ? (
+        <ul>
+          {subtasks.map((st, i) => (
+            <li key={`${i}-${st.title}`}>
+              <input type="checkbox" id={`${st.title}`} />
+              <label
+                htmlFor={`${st.title}`}
+                className={st.complete ? styles.complete : ""}
+              >
+                {st.title}
+              </label>
+            </li>
+          ))}
+        </ul>
+      ) : null}
+    </div>
+  );
+}
+
+export default Task;
