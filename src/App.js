c165173d3e0bcac2ba8a10dee7212f9ce0dc577b
habenz <haben.zaidt@gmail.com>
2025-01-09 23:23:19 -0500

    fix jiggle when scroll bar appears issue
    100 vw omits the area needed by the scrollbar https://stackoverflow.com/a/54577012

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const closingChecklist = initializeChecklist(tasks.Closing);
 
 function App() {
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex flex-col items-center bg-blue-200 w-screen">
       <h1 className="max-w-2xl w-4/5 px-1 text-xl sm:text-2xl font-bold">
         Closing Checklist
       </h1>
