0cda81e95d27cc56b4d6cabfe44e85a5b7fca95b
habenz <haben.zaidt@gmail.com>
2025-01-10 00:31:33 -0500

    use icon for subtask checkboxes

--- src/Checkbox.js
+++ src/Checkbox.js
@@ -1,14 +1,20 @@
 import { MdCheckBox, MdCheckBoxOutlineBlank } from "react-icons/md";
 
-function Checkbox({ checked, ariaLabel, onChange }) {
+function Checkbox({ size = "large", checked, ariaLabel, onChange, disabled }) {
+  const sizeClasses = {
+    small: "text-l",
+    large: "text-xl",
+  };
+
   return (
-    <label className="cursor-pointer text-xl">
+    <label className={`cursor-pointer ${sizeClasses[size]}`}>
       <input
         type="checkbox"
         className="hidden"
         checked={checked}
         aria-label={ariaLabel}
         onChange={onChange}
+        disabled={disabled}
       />
       {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
     </label>
--- src/SubtaskList.js
+++ src/SubtaskList.js
@@ -1,18 +1,20 @@
+import Checkbox from "./Checkbox";
+
 function SubtaskList({ subtasks, toggleCompletion, taskComplete }) {
   return (
     <ul
-      className="list-none ps-2 
+      className="list-none ps-8 
                 border rounded-b-lg border-gray-200 
                 bg-white bg-opacity-50"
     >
       {subtasks.map((st) => (
-        <li key={st.id}>
-          <input
-            type="checkbox"
+        <li key={st.id} className="flex items-center gap-1 py-0.5">
+          <Checkbox
             id={`${st.title}`}
             checked={st.complete || taskComplete}
             onChange={() => toggleCompletion(st.id)}
             disabled={taskComplete}
+            size="small"
           />
           <label
             htmlFor={`${st.title}`}
